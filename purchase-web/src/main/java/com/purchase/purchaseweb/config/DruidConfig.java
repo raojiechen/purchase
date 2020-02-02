0c70648063c67611697d47a67a818e91d13b1546
ranqing <ranqing@bingosoft.net>
2020-02-02 12:18:18 +0800

    调整了数据源配置文件位置，将dao层的数据源和mybatis配置移动到了web层。这样就可以访问druid监控。并没有删除dao层的数据源是因为方便打包不会报错

---
+++ purchase-web/src/main/java/com/purchase/purchaseweb/config/DruidConfig.java
@@ -0,0 +1,54 @@
+package com.purchase.purchaseweb.config;
+
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+
+
+@Configuration
+public class DruidConfig {
+
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Bean
+    public DataSource druidDataSourse(){
+        return new DruidDataSource();
+    }
+    //后台监控
+    @Bean
+    public ServletRegistrationBean statViewServlet(){
+        ServletRegistrationBean bean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+        //后台需要有人登陆，账号密码配置
+        HashMap<String,String> initParameters=new HashMap<>();
+
+        //增加配置
+        initParameters.put("loginUsername","admin");//登陆key是固定的
+        initParameters.put("loginPassword","123456");
+
+        //允许谁可以访问
+        initParameters.put("allow","");
+        //禁止谁能访问
+        //initParameters.put("aa","192.168.2.2")
+        bean.setInitParameters(initParameters);//设置初始化参数
+        return bean;
+    }
+
+    @Bean
+    public FilterRegistrationBean druidStatFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
+        /** 过滤规则 */
+        filterRegistrationBean.addUrlPatterns("/*");
+        /** 忽略资源 */
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+    }
+
+}
--- purchase-web/src/main/java/com/purchase/purchaseweb/controller/LoginController.java
+++ purchase-web/src/main/java/com/purchase/purchaseweb/controller/LoginController.java
@@ -1,14 +1,25 @@
 package com.purchase.purchaseweb.controller;
 
+import com.cqnu.purchasedao.mapper.StudentMapper;
+import com.cqnu.purchasedao.pojo.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
 public class LoginController {
+    @Autowired(required = false)
+    private StudentMapper studentMapper;
     @RequestMapping(value = "/login")
     public String login() {
+        List<Student> stuList=studentMapper.queryStuList();
+        for (Student student:stuList){
+            System.out.println(student);
+        }
         return "views/login";
     }
 }
